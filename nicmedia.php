4dd723e8febabf54f7471aedb4f5737df57b1a51
Preston Howell <pshowell23@gmail.com>
2019-05-17 16:59:35 -0400

    Content page styling

--- nicmedia.php
+++ nicmedia.php
@@ -10,7 +10,31 @@
         </header>
 
         <main>
-
+            <div class="container">
+                <nav>
+                    <a href="index.php">Home</a> / NicMedia
+                </nav>
+                <section class="content">
+                    <h2 class="contentTitle">NICMEDIA</h2>
+                    <div class="contentImage">
+                        <div style="width: 100%; height: 20em; background-color: black;"></div>
+                    </div>
+                    <div class="contentContent">
+                        <p>The NicMedia area of the library offers resources such as scanning and high-quality prints; lamination and binding; and digital video and audio editing equipment. Services available in NicMedia include media duplication and wide-format poster printing.<p>
+                    </div>
+                    <div class="contentList">
+                        <dl>
+                            <div class="questionGroup">
+                                <dt>Where is NicMedia?</span><span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                <dd>NicMedia is located on the ground floor of the Nicholson Library. Once you enter the library, head straight back and you will see NicMedia. If you would like to see a map, check out the <a href="#">ground-floor floor plan</a> of the library.</dd>
+                            </div>
+                            <div class="questionGroup">
+                                <dt>What resources and services are available in NicMedia?<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                <dd>The NicMedia area of the library offers resources such as scanning and high-quality prints; lamination and binding; and digital video and audio editing equipment. Services available in NicMedia include Media duplication and wide-format poster printing.</dd>
+                            </div>
+                        </dl>
+                </section>
+            </div>
         </main>
 
         <footer>
