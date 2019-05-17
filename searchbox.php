5e21758eb463927a7b564717384496b9d49b1baa
Preston Howell <pshowell23@gmail.com>
2019-05-17 12:16:56 -0400

    Add links under Searchbox

--- searchbox.php
+++ searchbox.php
@@ -44,4 +44,18 @@
             <?php include 'assets/search-box-tabs/mediaTab.php'; ?>
         </div>
     </div>
+
+    <!-- Links under Searchbox -->
+    <div class="searchLinks">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td><a href="https://anderson.on.worldcat.org/myaccount" target="_blank">My Library Account</a></td>
+                    <td><a href="#" onclick="window.open('https://youtu.be/uipNoxYbH4Q', 'tutorial1', 'width=425, height=349, left=300, top=80, menubar=0, toolbar=0, scrollbars=1, location=0, directories=0, resizable=1, status=0'); return false">Search Tutorial</a></td>
+                    <td><a href="http://anderson.worldcat.org/wcpa/courseReserves?action=courseReserveManager" target="_blank">Course Reserves</a></td>
+                    <td><a href="http://anderson.on.worldcat.org/advancedsearch?databaseList=1708,1920,2513,197,2175,1978,1736,638,283,1279,4162" target="_blank">Advanced Search</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
