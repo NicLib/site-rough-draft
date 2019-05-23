944895455304a1560612fb9e59e362ae0b0d613f
Preston Howell <pshowell23@gmail.com>
2019-05-23 11:09:39 -0400

    Add Hours template

---
+++ hours.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include 'head.php'; ?>
+        <title>Nicholson Library | Anderson University</title>
+    </head>
+    <body>
+        <header>
+            <?php include 'header.php'; ?>
+        </header>
+
+        <main>
+            <div class="container">
+                <nav>
+                    <a href="index.php">Home</a> / Hours
+                </nav>
+                <section class="content">
+                    <h2 class="contentTitle">Library Hours</h2>
+                    <div class="contentImage">
+                        <div style="width: 100%; height: 20em; background-color: black;"></div>
+                    </div>
+                    <div class="contentContent">
+                        <div id="hours-today" class="seeThis">
+                            <h3>Today's Hours:</h3>
+                            <p>7:45am - Midnight</p>
+                        </div>
+                        <div id="regular-hours">
+                            <div id="semester-hours" class="box box-hours">
+                                <table class="table">
+                                    <thead>
+                                        <th colspan=2>Semester Hours</th>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan=2>Computer lab accessible 24 hours with valid AU ID</td>
+                                        </tr>
+                                    <tbody>
+                                        <tr>
+                                            <td>Monday - Thursday</td>
+                                            <td>7:45am - Midnight</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Friday</td>
+                                            <td>7:45am - 5pm</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Saturday</td>
+                                            <td>11am - 5pm</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sunday</td>
+                                            <td>1:30pm - Midnight</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div id="summer-hours" class="box box-hours">
+                                <table class="table">
+                                    <thead>
+                                        <th colspan=2>Summer Hours</th>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan=2>Computer lab accessible from 5pm - Midnight with valid AU ID</td>
+                                        </tr>
+                                    <tbody>
+                                        <tr>
+                                            <td>Monday - Friday</td>
+                                            <td>8am - 5pm</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Saturday & Sunday</td>
+                                            <td>CLOSED</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    &nbsp;
+                    <div class="contentList">
+                        <dl>
+                            <div class="questionGroup">
+                                <dt>Fall Semester 2019 Holidays<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                <dd>
+                                    <div class="box-hours">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th colspan=2>Fall 2018</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>September 3 - Labor Day</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Septerber 29 - Homecoming</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>October 11 - Mid Fall Holiday</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>October 12-13 - Mid Fall Holiday</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 20 - Thanksgiving</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 21 - Thanksgiving</td>
+                                                    <td>8am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 22-24 - Thanksgiving</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 25 - Thanksgiving</td>
+                                                    <td>7pm - Midnight</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>December 14 - Last Day of Semester</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </dd>
+                            </div>
+                            <div class="questionGroup">
+                                <dt>Spring Semester 2020 Holidays<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                <dd>
+                                    <div class="box-hours">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th colspan=2>Fall 2018</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>September 3 - Labor Day</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Septerber 29 - Homecoming</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>October 11 - Mid Fall Holiday</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>October 12-13 - Mid Fall Holiday</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 20 - Thanksgiving</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 21 - Thanksgiving</td>
+                                                    <td>8am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 22-24 - Thanksgiving</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 25 - Thanksgiving</td>
+                                                    <td>7pm - Midnight</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>December 14 - Last Day of Semester</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </dd>
+                            </div>
+                            <div class="questionGroup">
+                                <dt>Summer 2020 Holidays<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                <dd>
+                                    <div class="box-hours">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th colspan=2>Fall 2018</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>September 3 - Labor Day</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Septerber 29 - Homecoming</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>October 11 - Mid Fall Holiday</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>October 12-13 - Mid Fall Holiday</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 20 - Thanksgiving</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 21 - Thanksgiving</td>
+                                                    <td>8am - 5pm</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 22-24 - Thanksgiving</td>
+                                                    <td>CLOSED</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>November 25 - Thanksgiving</td>
+                                                    <td>7pm - Midnight</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>December 14 - Last Day of Semester</td>
+                                                    <td>7:45am - 5pm</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                </section>
+            </div>
+        </main>
+
+        <footer>
+            <?php include 'footer.php' ?>
+        </footer>
+
+        <?php include 'add-js.php'; ?>
+    </body>
+</html>
