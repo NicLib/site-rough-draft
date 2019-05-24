266cfcd5ed15387a71b80dfc8cda50de097de5d6
Preston Howell <pshowell23@gmail.com>
2019-05-24 13:49:28 -0400

    Design staff page

--- staff.php
+++ staff.php
@@ -10,7 +10,189 @@
         </header>
 
         <main>
-
+            <div class="container">
+                <nav>
+                    <a href="index.php">Home</a> / Staff
+                </nav>
+                <section class="content">
+                    <h2 class="contentTitle">Staff</h2>
+                    <div class="contentContent">
+                        <h3 class="staff-title">Librarians</h3>
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Janet_Brewer.jpg" alt="Janet Brewer, Library Director">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Janet Brewer</h4>
+                                            <p>
+                                                <em><span class="job-title">Library Director</span></em><br/>
+                                                BA, Anderson College<br/>
+                                                MS, University of Illinois<br/>
+                                                MSLS, Simmons College<br/>
+                                                PhD, University of Kentucky
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:jlbrewer@anderson.edu">Email Janet</a><br/>
+                                                    Phone: <a href="tel:7656414272">(765) 641-4272</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/James_Bell.jpg" alt="James Bell, Reference Librarian">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">James Bell</h4>
+                                            <p>
+                                                <em><span class="job-title">Reference Librarian</span></em><br/>
+                                                BA, Azusa Pacific University<br/>
+                                                MLS, Indiana University
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:jcbell@anderson.edu">Email James</a><br/>
+                                                    Phone: <a href="tel:7656414281">(765) 641-4281</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Jeff_Siemon.jpg" alt="Jeff Siemon, Electronic Resources Librarian">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Jeff Siemon</h4>
+                                            <p>
+                                                <em><span class="job-title">Electronic Resources Librarian</span></em><br/>
+                                                BS, Carnegie-Mellon University<br/>
+                                                MDiv, Princeton Theological Seminary<br/>
+                                                MLS, University of Pittsburgh
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:josiemon@anderson.edu">Email Jeff</a><br/>
+                                                    Phone: <a href="tel:7656414277">(765) 641-4277</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Christa_Welty.jpg" alt="Christa Welty, Metadata/Reference Librarian">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Christa Welty</h4>
+                                            <p>
+                                                <em><span class="job-title">Metadata/Reference Librarian</span></em><br/>
+                                                BA, Cornerstone University<br/>
+                                                MLS, Indiana University
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:cjwelty@anderson.edu">Email Christa</a><br/>
+                                                    Phone: <a href="tel:7656414276">(765) 641-4276</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr colspan=2>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Heather_Myers.jpg" alt="Heather Myers, Public Services Librarian">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Heather Myers</h4>
+                                            <p>
+                                                <em><span class="job-title">Public Services Librarian</span></em><br/>
+                                                BA, Messiah College<br/>
+                                                MLS, Indiana University
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:hnmyers@anderson.edu">Email Heather</a><br/>
+                                                    Phone: <a href="tel:7656414288">(765) 641-4288</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3 class="staff-title">Staff</h3>
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Lori_Scott.jpg" alt="Lori Scott, Acquisitions">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Lori Scott</h4>
+                                            <p>
+                                                <em><span class="job-title">Aquisitions</span></em><br/>
+                                                AA, Anderson University
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:llscott@anderson.edu">Email Lori</a><br/>
+                                                    Phone: <a href="tel:7656414279">(765) 641-4279</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Preston_Howell.jpg" alt="Preston Howell, Library Technology Specialist">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Preston Howell</h4>
+                                            <p>
+                                                <em><span class="job-title">Library Technology Specialist</span></em><br/>
+                                                AA, Taylor University
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:pshowell@anderson.edu">Email Preston</a><br/>
+                                                    Phone: <a href="tel:7656414270">(765) 641-4270</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr colspan=2>
+                                    <td>
+                                        <div class="staff-img">
+                                            <img src="assets/images/staff/Nic_Don_Roark.jpg" alt="Nic Don Stanton-Roark, Archivist">
+                                        </div>
+                                        <div class="staff-info">
+                                            <h4 class="staff-name">Nic Don Stanton-Roark</h4>
+                                            <p>
+                                                <em><span class="job-title">Archivist</span></em><br/>
+                                                BS, Mid-America Christian University<br/>
+                                                MTS, Anderson University
+                                            </p>
+                                            <div class="staff-contact-info">
+                                                <p>
+                                                    <a href="mailto:ndroark@anderson.edu">Email Nic Don</a><br/>
+                                                    Phone: <a href="tel:7656414285">(765) 641-4285</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
         </main>
 
         <footer>
