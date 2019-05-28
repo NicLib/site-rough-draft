ce6388b466ad3e3f09d04f3190af7c0f5acdea12
Preston Howell <pshowell23@gmail.com>
2019-05-28 14:58:15 -0400

    Add Finding Aids page

---
+++ finding-aids.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include 'head.php'; ?>
+        <title>Nicholson Library | Anderson University</title>
+    </head>
+    <body>
+        <header>
+            <?php include 'header.php'; ?>
+        </header>
+
+        <main>
+            <div class="container">
+                <nav>
+                    <a href="index.php">Home</a> / <a href="archives.php">Archives</a> / Finding Aids
+                </nav>
+                <section class="content">
+                    <h2 class="contentTitle">Finding Aids</h2>
+                    <div class="contentImage">
+                        <div style="width: 100%; height: 20em; background-color: black;"></div>
+                    </div>
+                    <div class="contentContent">
+                        <p>
+                            <em>description about finding aids</em>
+                        </p>
+                        <p>
+                            <em>list of finding aids</em>
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </main>
+
+        <footer>
+            <?php include 'footer.php' ?>
+        </footer>
+
+        <?php include 'add-js.php'; ?>
+    </body>
+</html>
