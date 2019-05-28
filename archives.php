474bc684fabd55ae7a093487801666e8b5ff445e
Preston Howell <pshowell23@gmail.com>
2019-05-28 14:36:23 -0400

    Design Archives page

--- archives.php
+++ archives.php
@@ -20,7 +20,69 @@
                         <div style="width: 100%; height: 20em; background-color: black;"></div>
                     </div>
                     <div class="contentContent">
-
+                        <div id="hours-today" class="seeThis">
+                            <h3>Today's Hours:</h3>
+                            <p>9:30am - 12pm</p>
+                            <p>1:00pm - 2:30pm</p>
+                        </div>
+                        <p>Anderson University and the Church of God Movement (Anderson, IN) have a rich heritage. The Anderson University Archives focuses on collecting, organizing and preserving unpublished material, such as letters, manuscripts, and reports, so that they may be available for research. The Anderson University Archives maintains two areas of emphasis: Anderson University and the Church of God Movement (Anderson, IN).</p>
+                    </div>
+                    <div class="contentList">
+                        <div class="contentList">
+                            <dl>
+                                <div class="questionGroup">
+                                    <dt>Anderson University Collection<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                    <dd>
+                                        <p>The Anderson University Collection emphasizes the official records of Anderson University from its 1917 inception as Anderson Bible Training School to the present. Notable collections include:</p>
+                                        <ul>
+                                            <li>President John A Morrison and Robert H Reardon Papers</li>
+                                            <li>Dean Russell Olt</li>
+                                            <li><em>Anderson College News</em> and <em>The Andersonian</em></li>
+                                            <li>SOT Faculty Papers: Adam Miller & Gene Newberry</li>
+                                        </ul>
+                                    </dd>
+                                </div>
+                                <div class="questionGroup">
+                                    <dt>Church of God Movement (Anderson, IN) Collection<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                    <dd>
+                                        <p>The Church of God Movement (Anderson, IN) Collection preserves official records and person papers related to the history and development of the Church of God Reformation Movement. Notable collections include:
+                                        </p>
+                                        <ul>
+                                            <li>Early journals of D.S. Warner (1872-1880)</li>
+                                            <li>Papers of Barney E Warren, early Church of God hymn writer</li>
+                                            <li>F.G. Smith, Gospel Trumpet editor (1916-1930)</li>
+                                            <li>Early records of the Gospel Trumpet Company</li>
+                                        </ul>
+                                        <p>
+                                            This collection also includes congregational and regional histories, books written by Church of God authors, books published by Gospel Trumpet Company/Warner Press, Church of God pamphlets, Christian education curriculum, and national and regional periodical such as the <em>Gospel Trumpet<em>.
+                                        </p>
+                                        <p>
+                                            Because the Church of God is congregational in nature, there are very few records of genealogical interest.
+                                        </p>
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="contentContact">
+                        <h3>Contact</h3>
+                        <div class="staff-img">
+                            <img src="assets/images/staff/Nic_Don_Roark.jpg" alt="Nic Don Stanton-Roark, Archivist">
+                        </div>
+                        <div class="staff-info">
+                            <h4 class="staff-name">Nic Don Stanton-Roark</h4>
+                            <p>
+                                <em><span class="job-title">Archivist</span></em><br/>
+                                BS, Mid-America Christian University<br/>
+                                MTS, Anderson University
+                            </p>
+                            <div class="staff-contact-info">
+                                <p>
+                                    <a href="mailto:ndroark@anderson.edu">Email Nic Don</a><br/>
+                                    Phone: <a href="tel:7656414285">(765) 641-4285</a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </div>
