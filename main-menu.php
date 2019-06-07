59a847f6ea5b05be7f3ec7b9b71a851dc92363db
Preston Howell <pshowell23@gmail.com>
2019-06-07 11:39:28 -0400

    Organize files to make my life easier

---
+++ main-menu.php
@@ -0,0 +1,61 @@
+<div class="main-menu">
+    <div class="justify-content-end">
+        <ul class="nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Student Services</a>
+                <div class="dropdown-menu dropdown-menu-lg-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="nicmedia.php">Printing Services</a>
+                    <a class="dropdown-item" href="ill.php" style="text-decoration: line-through;">Interlibrary Loan</a>
+                    <a class="dropdown-item" href="https://libguides.anderson.edu" target="_blank">Research Help</a>
+                    <a class="dropdown-item" href="https://libguides.anderson.edu/az.php" target="_blank">Library Databases</a>
+                    <a class="dropdown-item" href="https://anderson.on.worldcat.org/courseReserves/landing" target="_blank">Reserves for Classes</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="contact.php">Contact a Librarian</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Faculty Support</a>
+                <div class="dropdown-menu dropdown-menu-lg-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Reserves Information</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Library Instruction</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Item Purchase</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Equity Grant</a>
+                    <a class="dropdown-item" href="https://illiad.anderson.edu" target="_blank">ILLiad</a>
+                    <div class="dropdown-divider"></div>
+                    <h5 class="dropdown-item">Recommended Resources</h5>
+                    <a class="dropdown-item" href="http://libproxy.anderson.edu/login?url=http://www.chronicle.com/" target="_blank">The Chronicle of Higher Education</a>
+                    <a class="dropdown-item" href="http://libproxy.anderson.edu/login?url=http://www.magnapubs.com/newsletter/the-teaching-professor/" target="_blank">The Teaching Professor</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="contact.php">Contact a Librarian</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Archives</a>
+                <div class="dropdown-menu dropdown-menu-lg-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="archives.php">Archives Home Page</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">AU & CHOG Digital Archive</a>
+                    <a class="dropdown-item" href="finding-aids.php">Finding Aids</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Research & Copies Information</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="contact.php">Contact a Librarian</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+                <div class="dropdown-menu dropdown-menu-lg-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="staff.php">Library Staff</a>
+                    <a class="dropdown-item" href="hours.php">Hours</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Mission & Vision</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Policies</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">Floor Plans</a>
+                    <a class="dropdown-item" href="#" style="text-decoration: line-through;">York Children's Literature Festival</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="contact.php">Contact a Librarian</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <i class="fas fa-search"></i>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ mobile-menu.php
@@ -0,0 +1,40 @@
+<div class="mobile-menu collapse" id="navbarSupportedContent">
+    <ul>
+        <li class="mobile-menu-item">Student Services <span class="menu-expand-icon"><i class="fas fa-caret-down"></i></span>
+            <ul class="mobile-menu-item-child">
+                <li>Printing Services</li>
+                <li>Interlibrary Loan</li>
+                <li>Research Help</li>
+                <li>Library Databases</li>
+                <li>Reserves for Classes</li>
+            </ul>
+        </li>
+        <li class="mobile-menu-item">Faculty Support <span class="menu-expand-icon"><i class="fas fa-caret-down"></i></span>
+            <ul class="mobile-menu-item-child">
+                <li>Printing Services</li>
+                <li>Interlibrary Loan</li>
+                <li>Research Help</li>
+                <li>Library Databases</li>
+                <li>Reserves for Classes</li>
+            </ul>
+        </li>
+        <li class="mobile-menu-item">Archives <span class="menu-expand-icon"><i class="fas fa-caret-down"></i></span>
+            <ul class="mobile-menu-item-child">
+                <li>Printing Services</li>
+                <li>Interlibrary Loan</li>
+                <li>Research Help</li>
+                <li>Library Databases</li>
+                <li>Reserves for Classes</li>
+            </ul>
+        </li>
+        <li class="mobile-menu-item">About <span class="menu-expand-icon"><i class="fas fa-caret-down"></i></span>
+            <ul class="mobile-menu-item-child">
+                <li>Printing Services</li>
+                <li>Interlibrary Loan</li>
+                <li>Research Help</li>
+                <li>Library Databases</li>
+                <li>Reserves for Classes</li>
+            </ul>
+        </li>
+    </ul>
+</div>
