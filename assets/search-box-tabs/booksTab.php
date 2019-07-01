157dea51d50b74444b6e93434554506927253c6b
Preston Howell <pshowell23@gmail.com>
2019-07-01 13:01:19 -0400

    Make search term stay consistent when switching tabs

--- assets/search-box-tabs/booksTab.php
+++ assets/search-box-tabs/booksTab.php
@@ -1,6 +1,6 @@
 <form class="searchForm" action="https://anderson.on.worldcat.org/search?" target="_blank" method="GET">
         <label class="sr-only" for="queryString">Search Term</label>
-        <input placeholder="Search the Nicholson Library Catalog (Books Tab)" class="searchTerm" type="text" name="queryString">
+        <input id="query2"placeholder="Search the Nicholson Library Catalog (Books Tab)" class="searchTerm searchbox" type="text" name="queryString">
         <input type="hidden" name="databaseList" value="<?php echo $books_database_list; ?>">
         <label class="sr-only" for="scope">Library Select</label>
         <select class="searchScope" name="scope">
