e27e4b1f349cdf5297874edcd19c9527d69385ad
Preston Howell <pshowell23@gmail.com>
2019-07-01 12:54:57 -0400

    Add databases to searchbox

--- assets/search-box-tabs/everythingTab.php
+++ assets/search-box-tabs/everythingTab.php
@@ -1,6 +1,7 @@
 <form class="searchForm" action="https://anderson.on.worldcat.org/search?" target="_blank" method="GET">
     <label class="sr-only" for="queryString">Search Term</label>
     <input placeholder="Search the Nicholson Library Catalog (Everything Tab)" class="searchTerm" type="text" name="queryString">
+    <input type="hidden" name="databaseList" value="<?php echo $everything_database_list; ?>">
     <label class="sr-only" for="scope">Library Select</label>
     <select class="searchScope" name="scope">
         <option selected value>Libraries Worldwide</option>
--- assets/search-box-tabs/mediaTab.php
+++ assets/search-box-tabs/mediaTab.php
@@ -1,10 +1,11 @@
 <form class="searchForm" action="https://anderson.on.worldcat.org/search?" target="_blank" method="GET">
         <label class="sr-only" for="queryString">Search Term</label>
         <input placeholder="Search the Nicholson Library Catalog (Media Tab)" class="searchTerm" type="text" name="queryString">
+        <input type="hidden" name="databaseList" value="<?php echo $media_database_list; ?>">
         <label class="sr-only" for="scope">Library Select</label>
         <select class="searchScope" name="scope">
             <option selected value>Libraries Worldwide</option>
             <option value="<?php echo $oclc_niclib_scope; ?>">Nicholson Library</option>
         </select>
-    <button class="searchSubmit" type="submit"><i class="fas fa-search"></i></button>
+        <button class="searchSubmit" type="submit"><i class="fas fa-search"></i></button>
 </form>
