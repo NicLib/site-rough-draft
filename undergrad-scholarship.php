b14c9fa275356b45d767cc8a7e6eb2f2e8249658
Preston Howell <pshowell23@gmail.com>
2019-10-21 10:44:32 -0400

    Add undergrad scholarship page

---
+++ undergrad-scholarship.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include 'head.php'; ?>
+        <title>Undergraduate Scholarship | Anderson University</title>
+    </head>
+    <body>
+        <header>
+            <?php include 'header.php'; ?>
+        </header>
+
+        <main>
+            <div class="container">
+                <nav>
+                    <a href="index.php">Home</a> / Undergraduate Scholarship Collection
+                </nav>
+                <section class="content">
+                    <h2 class="contentTitle">Undergraduate Scholarship Collection</h2>
+                    <div class="contentImage">
+                        <div style="width: 100%; height: 20em; background-color: black;"></div>
+                    </div>
+                    <div class="contentContent">
+                        <p>At <a href="https://anderson.edu" target="_blank">Anderson University</a> we don't believe that learning -- or faith -- is dry, stuffy, or stale. Anderson student question... investigate... discover... interpret... arrive at new conclusions... share. Students roll up their sleeves and fully engage with the messy, sometimes paradoxical, world of research and the creative process. Our esteemed faculty members come alongside and mentor throughout the journey.</p>
+                        <p>The <a href="https://palni.contentdm.oclc.org/digital/collection/p15705coll17/search" target="_blank">AU Undergraduate Scholarship</a> collection showcases the "share" part of the process. Here you will find Senior Seminar Papers; Honors Theses; Senior Chapel Speeches; and abstracts of poster sessions from "Scholars Day."</p>
+                    </div>
+                    <div class="contentList">
+                        <dl>
+                            <div class="questionGroup">
+                                <dt>Do you have a submission?<span class="expand-icon"><i class="fas fa-plus-circle"></i></span></dt>
+                                <dd>
+                                    <p>Congratulations on completeing your honors thesis, senior seminar paper, baccalaureate address, or convocation/chapel address. In addition to the delivery of a print copy of your work to the Library Director (for placement in the <a href="archives.php" target="_blank">Anderson University & Church of God Archives</a>), all students will deposit a digital version of their work into <a href="https://palni.contentdm.oclc.org/digital/collection/p15705coll17/search" target="_blank">Anderson University's Digital Repository</a>.</p>
+                                    <p>The library preserves and provides access to your work in these ways:</p>
+                                    <ul>
+                                        <li>Each student's print copy is available to the members of the Anderson University community and the general public in the <a href="archives.php" target="_blank">Anderson University & Church of God Archives</a> during its regular business hours.</li>
+                                        <li>The digital, full-text version of each student's work is available to the members of the current Anderson University community when the work is depostited in the <a href="https://palni.contentdm.oclc.org/digital/collection/p15705coll17/search" target="_blank">Anderson University Digital Repository</a>, our web-based, open access compliant institutional repository for Anderson-produced digital content. It is a permanent archive.</li>
+                                        <li>The digital full-text version of the student's work may be made available globally to better serve the Anderson University community.</li>
+                                    </ul>
+                                    <p>Your work is discoverable in two ways:</p>
+                                    <ul>
+                                        <li>Every student's work is placed in the Nicholson Library catalog of resources. A link to the digital copy is included there, as well.</li>
+                                        <li>Every student's work deposited in the <a href="https://palni.contentdm.oclc.org/digital/collection/p15705coll17/search" target="_blank">Anderson University Digital Repository</a> is searchable using any internet search engine.</li>
+                                    </ul>
+                                    <h3>Questions?</h3>
+                                    <p>Contact Dr Janet Brewere in the Nicholson Library by <a href="mailto:jlbrewer@anderson.edu">email</a> or <a href="tel:7656414272">765-641-4272</a>. Bring the signed form to Janet Brewer in the Nicholson Library along with the print version; to submit your work to the Digital Repository, complete Part II of <a href="#">this form</a>.</p>
+                                    <p><b>Thank you for submitting your work. We appreciate your willingness to share your academic scholarship!</b></p>
+                                    <button class="btn btn-lg">Download the Consent Form</button>
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                    <div class="contentContact">
+                        <h3>Contact</h3>
+                        <div class="staff-img">
+                            <img src="assets/images/staff/Janet_Brewer.jpg" alt="Janet Brewer, Library Director">
+                        </div>
+                        <div class="staff-info">
+                            <h4 class="staff-name">Janet Brewer</h4>
+                            <p>
+                                <em><span class="job-title">Library Director</span></em><br/>
+                            </p>
+                            <div class="staff-contact-info">
+                                <p>
+                                    <a href="mailto:jlbrewer@anderson.edu">Email Janet</a><br/>
+                                    Phone: <a href="tel:7656414272">(765) 641-4272</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+
+        <footer>
+            <?php include 'footer.php' ?>
+        </footer>
+
+        <?php include 'add-js.php'; ?>
+    </body>
+</html>
